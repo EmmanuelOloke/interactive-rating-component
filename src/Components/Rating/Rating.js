ba398ff1c33717d4c14494bbbe78c46d208e0b0b
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-04-15 13:40:34 +0100

    styling for rating page done

--- src/Components/Rating/Rating.js
+++ src/Components/Rating/Rating.js
@@ -1,8 +1,41 @@
 import React from 'react';
+import Star from '../../images/icon-star.svg';
 import './Rating.css';
 
 export default function Rating() {
     return (
-        <div className="container">Just something to start with</div>
+        <div className="container">
+            <div className="star">
+                <img src={Star} alt="Star Icon" />
+            </div>
+
+            <h2>How did we do?</h2>
+
+            <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+
+            <div className="rates">
+                <div className="rate">
+                    1
+                </div>
+
+                <div className="rate">
+                    2
+                </div>
+
+                <div className="rate">
+                    3
+                </div>
+
+                <div className="rate">
+                    4
+                </div>
+
+                <div className="rate">
+                    5
+                </div>
+            </div>
+
+            <button className="submit">submit</button>
+        </div>
     );
 }
