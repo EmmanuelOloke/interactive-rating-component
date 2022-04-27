7b2142c1c205da755c19a15c53ed8508d26250fd
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-04-27 17:40:39 +0100

    implement functionality using React useState

--- src/Components/Rating/Rating.js
+++ src/Components/Rating/Rating.js
@@ -1,62 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Star from '../../images/icon-star.svg';
 import Thanks from '../../images/illustration-thank-you.svg';
 import './Rating.css';
 
 export default function Rating() {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [selected, setSelected] = useState(1);
 
-    const addActive = () => {
-        document.getElementsByClassName('rate').classList.remove('active');
-        // document.getElementById(id).classList.add("active");
-    }
+  const changeSelected = (num) => {
+    setSelected(num);
+  };
 
-    return (
-        <div className="container">
-            <div className="select">
-                <div className="star">
-                    <img src={Star} alt="Star Icon" />
-                </div>
+  const handleSubmit = () => {
+    setIsSubmitted(true);
+  };
 
-                <h2>How did we do?</h2>
+  return (
+    <div className="container">
+      {isSubmitted ? (
+        <div className="thanks">
+          <img className="mb" src={Thanks} alt="Thank you icon" />
 
-                <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
-
-                <div className="rates">
-                    <span id="1" className="active rate" onClick={() => addActive()}>
-                        1
-                    </span>
-
-                    <span id="2" className="rate" onClick={() => addActive()}>
-                        2
-                    </span>
-
-                    <span id="3" className="rate" onClick={() => addActive()}>
-                        3
-                    </span>
-
-                    <span id="4" className="rate" onClick={() => addActive()}>
-                        4
-                    </span>
-
-                    <span id="5" className="rate" onClick={() => addActive()}>
-                        5
-                    </span>
-                </div>
-
-                <button className="submit">submit</button>
-            </div>
-
-            <div className="thanks">
-                <img className="mb" src={Thanks} alt="Thank you icon" />
-
-                <p className="selected">
-                    You selected 4 out of 5
-                </p>
-                <h2>Thank you!</h2>
-                <p>We appreciate you taking the time to give a rating.
-                    If you ever need more support, don't hesitate to
-                    get in touch!</p>
-            </div>
+          <p className="selected">You selected {selected} out of 5</p>
+          <h2>Thank you!</h2>
+          <p>
+            We appreciate you taking the time to give a rating. If you ever need
+            more support, don't hesitate to get in touch!
+          </p>
         </div>
-    );
+      ) : (
+        <div className="select">
+          <div className="star">
+            <img src={Star} alt="Star Icon" />
+          </div>
+
+          <h2>How did we do?</h2>
+
+          <p>
+            Please let us know how we did with your support request. All
+            feedback is appreciated to help us improve our offering!
+          </p>
+
+          <div className="rates">
+            <span
+              id="1"
+              className={`rate ${selected === 1 && 'active'}`}
+              onClick={() => changeSelected(1)}
+            >
+              1
+            </span>
+
+            <span
+              id="2"
+              className={`rate ${selected === 2 && 'active'}`}
+              onClick={() => changeSelected(2)}
+            >
+              2
+            </span>
+
+            <span
+              id="3"
+              className={`rate ${selected === 3 && 'active'}`}
+              onClick={() => changeSelected(3)}
+            >
+              3
+            </span>
+
+            <span
+              id="4"
+              className={`rate ${selected === 4 && 'active'}`}
+              onClick={() => changeSelected(4)}
+            >
+              4
+            </span>
+
+            <span
+              id="5"
+              className={`rate ${selected === 5 && 'active'}`}
+              onClick={() => changeSelected(5)}
+            >
+              5
+            </span>
+          </div>
+
+          <button className="submit" onClick={handleSubmit}>
+            submit
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
